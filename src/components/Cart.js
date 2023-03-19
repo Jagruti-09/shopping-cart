cfb2efc13ef4c8bb8a076149da83f71e3efc9f6f
Jagruti-Bhat <jagrutibhat093@gmail.com>
2023-03-19 13:15:04 +0530

    react-context-api

---
+++ src/components/Cart.js
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { Cart } from '../Context';
+import SingleProduct from './SingleProduct'
+
+const CartPage = () => {
+
+  
+  const [total,setTotal] = useState();
+
+  const {cart} = useContext(Cart);
+
+  useEffect(() => {
+    setTotal(cart.reduce((acc,curr) => acc+Number(curr.price),0))
+  }, [cart]);
+  return (
+
+    <div>
+      <span style={{fontSize:30}}>My Cart</span>
+      <br/>
+      <span style={{fontSize:30}}>Total: Rs. {total}</span>
+    <div className='productContainer'>
+     {cart.map((prod) => (
+      <SingleProduct prod={prod} key={prod.id}/>
+
+     ) )}
+    </div>
+    </div>
+  )
+}
+
+export default CartPage;
